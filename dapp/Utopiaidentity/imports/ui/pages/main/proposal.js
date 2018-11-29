39e7f34f3c7677b184e75a10d43e2d800325ae8a
kstvigilante <kst85077@gmail.com>
2018-11-29 17:54:42 +0530

    created sid branch

---
+++ dapp/Utopiaidentity/imports/ui/pages/main/proposal.js
@@ -0,0 +1,115 @@
+import "./proposal.html"
+import "../../stylesheets/proposal.css";
+import Eos from "eosjs";
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+var count = 0;
+
+eosConfig = {
+    chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473", // 32 byte (64 char) hex string
+    keyProvider: ['5Jur4pK1Rb8xvdfNUUZJq5JE36HQUd9PNouWwjUdbWw7cK8ZuUo'],
+    // WIF string or array of keys..
+    httpEndpoint: 'https://jungle2.cryptolions.io:443',
+    expireInSeconds: 60,
+    broadcast: true,
+    verbose: false, // API activity
+    sign: true
+}
+const eos = Eos(eosConfig);
+
+Template.App_proposal.onRendered(async function () {
+    let tabledata = await eos.getTableRows({
+        code: "voteproposal",
+        scope: "voteproposal",
+        table: 'proposal11',
+        limit: 50,
+        json: true,
+    });
+    document.getElementById("proposal-group").innerHTML = "";
+   
+
+    for (var i = 0; i < tabledata.rows.length; i++) {
+        var desc = tabledata.rows[i].proposal_description;
+        var votebutton = "votebutton";
+        var resultbutton = "resultbutton";
+        votebutton = votebutton + tabledata.rows[i].id;
+        resultbutton = resultbutton + tabledata.rows[i].id;
+        document.getElementById("proposal-group").innerHTML +=
+            "<div class = 'redo'><p>" + desc + "</p><button class = 'vote-button' id = '" + votebutton + "'>vote</button>"
+            + "<button class = 'result-button' id = '" + resultbutton + "'>result</button>" + "</div>";
+
+    }
+})
+
+Template.App_proposal.events({
+    "click .new-proposal-button": function () {
+        /* document.getElementById("form-section").style.display = "block";
+        document.getElementById("all-proposals").style.display = "none"; */
+        FlowRouter.go("/newproposal");
+    },
+    "click .all-proposal-button": async function () {
+        /* document.getElementById("form-section").style.display = "none";
+        document.getElementById("all-proposals").style.display = "block"; */
+        /* let tabledata = await eos.getTableRows({
+            code: "voteproposal",
+            scope: "voteproposal",
+            table: 'proposal13',
+            limit: 50,
+            json: true,
+        });
+        document.getElementById("proposal-group").innerHTML = "";
+        console.log("table data ", tabledata);
+        var id = 0;
+        for(var i = 0; i< tabledata.rows.length;i++){
+            var desc = tabledata.rows[i].proposal_description;
+            document.getElementById("proposal-group").innerHTML += 
+            "<div class = 'redo'><p>"+desc+"</p><button class = 'vote-button' id = '"+id+"'>vote</button>"+"</div>";
+            id = id+1;
+        } */
+
+    },
+    "click #options": function () {
+        /* var boxName = "textbox" + count;
+        var options = "Option";
+        document.getElementById("form-group").innerHTML += "<input type = 'text' placeholder = '"+ options +"' id = '"+boxName+"'/>"
+        count += 1;
+         */
+        /* '<input type="text" id="' + boxName + '" "  />'; */
+    },
+    "click #create-proposal": function () {
+
+        /* var proposal = $("#proposal").val();
+        var duration = parseInt($("#duration").val());
+        var noofwinners = parseInt($("#noofwinners").val());
+        var options = [];
+        for (var i = 0; i < count; i++) {
+            var textbox = "#textbox" + i;
+            var option = $(textbox).val();
+            options.push(option);
+        }
+        var username = localStorage.getItem("username")
+        eos.contract("voting").then(voting => {
+            voting.createprop(proposal, duration, options, username, noofwinners, { authorization: username }, (err, res) => {
+                if (err) {
+                    console.log("error ", err);
+                }
+                else {
+                    console.log("Result ", res);
+                }
+            })
+        }) */
+    },
+    "click .vote-button": function (event) {
+        event.preventDefault();
+        var id = event.target.id;
+        id = id[id.length - 1];
+
+        FlowRouter.go("/vote/" + id);
+    },
+    "click .result-button": function (event) {
+        event.preventDefault();
+        var id = event.target.id;
+        id = id[id.length - 1];
+        FlowRouter.go("/result/" + id);
+    }
+})
---
+++ dapp/Utopiaidentity/imports/ui/pages/main/welocomePage.js
@@ -0,0 +1,119 @@
+import "./welcomePage.html";
+import "../../stylesheets/utopiaIdentity.css";
+import "../../pages/main/header.js";
+import "../../pages/main/footer.js"
+import ScatterJS from "scatterjs-core";
+import ScatterEOS from "scatterjs-plugin-eosjs";
+import Eos from "eosjs";
+ScatterJS.plugins(new ScatterEOS());
+const network = {
+    protocol: "https", // Defaults to https
+    blockchain: "eos",
+    host: "jungle2.cryptolions.io",
+    port: 443,
+    chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473"
+  };
+const eosOptions = {
+  chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473"
+};
+
+
+
+eosConfig = {
+    chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473", // 32 byte (64 char) hex string
+    keyProvider: ['5KeNdWYxPbUpsLUa8QT64AbjTAQeHcZejcR6shHnNi1sESgxgm7'],
+    // WIF string or array of keys..
+    httpEndpoint: 'https://jungle2.cryptolions.io:443',
+    expireInSeconds: 60,
+    broadcast: true,
+    verbose: false, // API activity
+    sign: true
+}
+eos = Eos(eosConfig)
+
+
+Template.welcomePage.onCreated(function bodyOnCreated() {
+        ScatterJS.scatter.connect('utopia').then((connected) => {
+            if (connected) {
+                if (ScatterJS.scatter.connect('utopia')) {
+                    scatter = ScatterJS.scatter;
+                    const requiredFields = { accounts: [network] };
+                    const eos = scatter.eos(network, Eos, eosOptions);
+                    if (scatter.identity) {
+                        const acc = scatter.identity.accounts.find(x => x.blockchain === 'eos');
+                        const account = acc.name;
+                        localStorage.setItem("loginstatus",JSON.stringify(true)); 
+                        localStorage.setItem("username",account);                       
+                        console.log("inside created----1",localStorage.getItem("loginstatus"));
+                        document.getElementById("loginButton").innerHTML = "logout";
+                        document.getElementsByClassName("optionFlex")[0].style.display="flex";
+
+                    } else {
+                        localStorage.setItem("loginstatus",JSON.stringify(false));
+                        localStorage.setItem("username","");
+                        console.log("inside created----2",localStorage.getItem("loginstatus"));
+                        document.getElementById("loginButton").innerHTML = "login";
+                        document.getElementsByClassName("optionFlex")[0].style.display="none";
+                    }
+                }
+            } else {
+                console.log("scatter not installed")
+            }
+        })
+   
+  });
+ 
+Template.welcomePage.events({
+    "click .optionText1": function() {
+        FlowRouter.go("/identity-reg",{data:"scatter"});
+      },
+    'click .scatterloginlogout': function( event, instance ){
+    if (!JSON.parse(localStorage.getItem("loginstatus"))) {
+        ScatterJS.scatter.connect('utopia').then(connected => {
+            if (!connected) return false;
+            scatter = ScatterJS.scatter;
+            const requiredFields = { accounts: [network] };
+            const eos = scatter.eos(network, Eos, eosOptions);
+            scatter.getIdentity(requiredFields).then(() => {
+                const acc = scatter.identity.accounts.find(x => x.blockchain === 'eos');
+                const account = acc.name
+                localStorage.setItem("username",account);
+                console.log("inlogin");
+                localStorage.setItem("loginstatus",JSON.stringify(true));
+                localStorage.setItem("username",account);
+                document.getElementById("loginButton").innerHTML = "logout";
+                document.getElementsByClassName("optionFlex")[0].style.display="flex";
+            }).catch(error => {
+                console.error(error);
+            });
+        });
+    } else {
+        console.log("2-----------------")
+        ScatterJS.scatter.forgetIdentity().then(() => {
+            localStorage.setItem("loginstatus",JSON.stringify(false));
+            console.log("----",localStorage.getItem("loginstatus"));
+            document.getElementById("loginButton").innerHTML = "login";
+            localStorage.setItem("username","");
+            console.log("logout");
+            document.getElementsByClassName("optionFlex")[0].style.display="none";
+        });
+    }
+    },
+})
+
+Template.welcomePage.events({
+      "click .optionText2":function(){
+        var username= localStorage.getItem("username");
+        eos.contract('identityreg1').then(identityreg1 => {
+          identityreg1.reqcitizen(username,{authorization:username}).then((response)=>{
+              if(response){
+                  console.log("hello--",response);
+              }else{
+                  alert("identity is not registered !!!!");;
+              }
+          });
+        
+        })
+        FlowRouter.go("/citizenship",{eosinstance :scatter});
+      }
+    })
