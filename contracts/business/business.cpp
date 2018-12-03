542d4f222af0de5aebd2593ac3f88cb31367e222
kstvigilante <kst85077@gmail.com>
2018-12-03 18:38:01 +0530

    changes to business smart contract

--- contracts/business/business.cpp
+++ contracts/business/business.cpp
@@ -1,69 +1,172 @@
 #include "business.hpp"
 
-ACTION business::addbusiness(name owner, string businessname){
+ACTION business::addbusiness(name owner, string businessname)
+{
+
     require_auth(owner);
+
+    identity_table idtb("identityreg1"_n, "identityreg1"_n.value);
     businesstb bt(_self, _self.value);
-    bt.emplace(_self, [&](auto &c){
-        c.id = bt.available_primary_key();
+    auto eitr = idtb.find(owner.value);
+    eosio_assert(eitr != idtb.end(), "Not a member of Utopia community");
+
+    bt.emplace(_self, [&](auto &c) {
+        c.company_id = bt.available_primary_key();
         c.owner = owner;
         c.businessname = businessname;
+        c.status = CREATED;
     });
 }
 
-ACTION business::makepublic(uint64_t id, double marketcap, double totalshares){
+ACTION business::approve(uint64_t company_id)
+{
+
+    require_auth(_self);
     businesstb bt(_self, _self.value);
-    auto itr = bt.find(id);
-    require_auth(itr->owner);
+    auto itr = bt.find(company_id);
+    eosio_assert(itr != bt.end(), "No such company exist");
+
+    bt.modify(itr, _self, [&](auto &c) {
+        c.status = APPROVED;
+    });
+}
+
+ACTION business::makepublic(uint64_t company_id, asset maximum_supply)
+{
+
+    businesstb bt(_self, _self.value);
+    auto itr = bt.find(company_id);
+    require_auth(itr->owner); 
     eosio_assert(itr != bt.end(), "No such company exists");
-    eosio_assert(marketcap>0, "marketcap should be greater than 0");
-    eosio_assert(totalshares>0, "totalshares should be greater than 0");
-    eosio_assert(itr->marketcap == 0, "Company already public");
+    name issuer = itr->owner;
+
+    //creating token on utopiatoken smart contract
+    action(
+        permission_level{"amartesttest"_n, "active"_n},
+        "amartesttest"_n, "create"_n,
+        std::make_tuple(issuer,maximum_supply)
+    ).send();
 
     bt.modify(itr, _self, [&](auto &c){
-        c.marketcap = marketcap;
-        c.totalshares = totalshares;
-        c.shareprice = marketcap/totalshares;
-    });
-
-}
-
-ACTION business::newshareprice(uint64_t id, double newshareprice){
-    businesstb bt(_self, _self.value);
-    auto itr = bt.find(id);
-    eosio_assert(itr != bt.end(), "No such business exist");
-    eosio_assert(itr->marketcap > 0 ,"business not public yet");
-    eosio_assert(newshareprice >0 , "new share price should not be less than or equal to 0");
-
-    require_auth(itr->owner);
-
-    bt.modify(itr, _self, [&](auto& c){
-        c.shareprice = newshareprice;
-        c.marketcap = newshareprice * itr->totalshares;
+        c.token_maximum_supply = maximum_supply;
     });
 }
 
-ACTION business::newtotalshare(uint64_t id, double newtotalshare){
-    businesstb bt(_self, _self.value);
-    auto itr = bt.find(id);
-    eosio_assert(itr != bt.end(), "No such business exist");
-    eosio_assert(itr->marketcap > 0 ,"business not public yet");
-    eosio_assert(newtotalshare >0 ,"total shares cannot be 0 or less");
+ACTION business::deleteall()
+{
 
-    require_auth(itr->owner);
-
-    bt.modify(itr, _self, [&](auto& c){
-        c.totalshares = newtotalshare;
-        c.shareprice = itr->marketcap/newtotalshare;
-    });
-}
-
-ACTION business::deleteall(){
+    //delets all data/companies
     require_auth(_self);
     businesstb bt(_self, _self.value);
     auto itr = bt.begin();
-    while(itr != bt.end()){
+    while (itr != bt.end())
+    {
         itr = bt.erase(itr);
     }
 }
 
-EOSIO_DISPATCH(business, (addbusiness)(makepublic)(newshareprice)(newtotalshare)(deleteall))
+ACTION business::delcompany(uint64_t company_id)
+{
+
+    //delete one company
+    require_auth(_self);
+    businesstb bt(_self, _self.value);
+    auto itr = bt.find(company_id);
+    eosio_assert(itr != bt.end(), "Company does not exist");
+    itr = bt.erase(itr);
+}
+
+ACTION business::addemployee(uint64_t company_id, name employee)
+{
+    //searching for employee in utopia
+    identity_table idtb("identityreg1"_n, "identityreg1"_n.value);
+    businesstb bt(_self, _self.value);
+    auto eitr = idtb.find(employee.value);
+    eosio_assert(eitr != idtb.end(), "Not a member of Utopia community");
+    //searching for company
+    auto citr = bt.find(company_id);
+    eosio_assert(citr != bt.end(), "Company does not exist");
+    require_auth(citr->owner);
+    //searching for employee in your company if exists do not add
+    auto aditr = find(citr->employees.begin(), citr->employees.end(), employee);
+    eosio_assert(aditr == citr->employees.end(), "Employee already exists");
+    //add employee to your company
+    bt.modify(citr, _self, [&](auto &c) {
+        c.employees.push_back(employee);
+    });
+}
+
+ACTION business::rmemployee(uint64_t company_id, name employee)
+{
+    //searching for employee in utopia
+    identity_table idtb("identityreg1"_n, "identityreg1"_n.value);
+    businesstb bt(_self, _self.value);
+    auto eitr = idtb.find(employee.value);
+    eosio_assert(eitr != idtb.end(), "Not a member of Utopia community");
+    //searching for company
+    auto citr = bt.find(company_id);
+    eosio_assert(citr != bt.end(), "Company does not exist");
+    require_auth(citr->owner);
+    //searching for employee in your company
+    auto rmeitr = find(citr->employees.begin(), citr->employees.end(), employee);
+    eosio_assert(rmeitr != citr->employees.end(), "No such employee exists in your company");
+    //removing the employee from your company
+    bt.modify(citr, _self, [&](auto &c) {
+        c.employees.erase(rmeitr);
+    });
+}
+
+ACTION business::printnames()
+{
+    require_auth(_self);
+
+    identity_table id("identityreg1"_n, "identityreg1"_n.value);
+    auto itr = id.begin();
+    while (itr != id.end())
+    {
+        print(" --identity--> ", name{itr->username});
+        itr++;
+    }
+}
+
+ACTION business::adshareowner(uint64_t company_id, name shareholder)
+{
+
+    identity_table idtb("identityreg1"_n, "identityreg1"_n.value);
+    businesstb bt(_self, _self.value);
+    auto eitr = idtb.find(shareholder.value);
+    eosio_assert(eitr != idtb.end(), "Not a member of Utopia community");
+
+    auto citr = bt.find(company_id);
+    eosio_assert(citr != bt.end(), "Company does not exist");
+    require_auth(citr->owner);
+
+    auto aditr = find(citr->shareholders.begin(), citr->shareholders.end(), shareholder);
+    eosio_assert(aditr == citr->shareholders.end(), "Shareholder already exists");
+
+    bt.modify(citr, _self, [&](auto &c) {
+        c.shareholders.push_back(shareholder);
+    });
+}
+
+ACTION business::rmshareowner(uint64_t company_id, name shareholder)
+{
+
+    identity_table idtb("identityreg1"_n, "identityreg1"_n.value);
+    businesstb bt(_self, _self.value);
+    auto eitr = idtb.find(shareholder.value);
+    eosio_assert(eitr != idtb.end(), "Not a member of Utopia community");
+
+    auto citr = bt.find(company_id);
+    eosio_assert(citr != bt.end(), "Company does not exist");
+    require_auth(citr->owner);
+
+    auto rmeitr = find(citr->shareholders.begin(), citr->shareholders.end(), shareholder);
+    eosio_assert(rmeitr != citr->shareholders.end(), "No such shareholder exists in your company");
+    //removing the employee from your company
+    bt.modify(citr, _self, [&](auto &c) {
+        c.shareholders.erase(rmeitr);
+    });
+}
+
+EOSIO_DISPATCH(business, (addbusiness)(approve)(makepublic)(deleteall)(delcompany)(addemployee)(rmemployee)(printnames)(adshareowner)(rmshareowner))
--- contracts/business/business.hpp
+++ contracts/business/business.hpp
@@ -5,35 +5,61 @@
 using namespace eosio;
 using namespace std;
 
-CONTRACT business : public contract{
+CONTRACT business : public contract
+{
 
-    public:
-        using contract::contract;
+  public:
+    using contract::contract;
 
-        ACTION addbusiness(name owner, string businessname);
+    ACTION addbusiness(name owner, string businessname);
 
-        ACTION makepublic(uint64_t id, double marketcap, double totalshares);
+    ACTION approve(uint64_t company_id);
 
-        ACTION newshareprice(uint64_t id, double newshareprice);
+    ACTION makepublic(uint64_t company_id, asset maximum_supply);
 
-        ACTION newtotalshare(uint64_t id, double newtotalshare);
+    ACTION deleteall();
 
-        ACTION deleteall();
+    ACTION delcompany(uint64_t company_id);
 
-        TABLE businessst{
-            uint64_t id;
-            name owner;
-            string businessname;
-            double marketcap;
-            double totalshares;
-            double shareprice;
-            double sharestart;
-            double shareend;
-            vector <string> officers;
-            vector <string> employees;
+    ACTION addemployee(uint64_t company_id, name employee);
 
-            uint64_t primary_key() const { return id; }
-        };
+    ACTION rmemployee(uint64_t company_id, name employee);
 
-        typedef multi_index<"businesstb"_n, businessst> businesstb;
+    ACTION adshareowner(uint64_t company_id, name shareholder);
+
+    ACTION rmshareowner(uint64_t company_id, name shareholder);
+
+    ACTION printnames();
+
+    uint8_t CREATED = 0;
+    uint8_t APPROVED = 1;
+
+    TABLE businessst
+    {
+        uint64_t company_id;
+        uint8_t status;
+        name owner;
+        string businessname;
+        asset token_maximum_supply;
+        vector<name> shareholders;
+        vector<name> employees;
+
+        uint64_t primary_key() const { return company_id; }
+    };
+
+    TABLE identityt
+    {
+        name username;
+        string fname;
+        string mname;
+        string lname;
+        string dob;
+        string contact;
+        string email;
+        bool citizen = false;
+        uint64_t primary_key() const { return username.value; }
+    };
+
+    typedef multi_index<"businesstb"_n, businessst> businesstb;
+    typedef multi_index<"identity2"_n, identityt> identity_table;
 };
