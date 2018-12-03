2e40e60ee400e064cfdbf3b49d5e5bd2034fbddc
Amar <amarjss0094@gmail.com>
2018-12-03 15:15:00 +0530

    changes

---
+++ contracts/eosio.token/CMakeFiles/eosio.token.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/admin/Documents/eos1.1/contracts/eosio.token/eosio.token.cpp" "/Users/admin/Documents/eos1.1/build/contracts/eosio.token/eosio.token.cpp.bc"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../contracts"
+  "../externals/magic_get/include"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
