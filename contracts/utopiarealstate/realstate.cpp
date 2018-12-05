d20f087f78fbc81ed2ee5cd6a06f2b1228526976
Amar <amarjss0094@gmail.com>
2018-12-05 15:06:00 +0530

    realstate contract change

--- contracts/utopiarealstate/realstate.cpp
+++ contracts/utopiarealstate/realstate.cpp
@@ -2,7 +2,7 @@
 
 ACTION realstate::landproposal(string location, uint64_t area, name currentOwner, asset currentprice, uint64_t startdate, uint64_t enddate)
 {
-    print("land proposal");
+    print("land proposal!!!");
     require_auth(currentOwner);
 
     bid_table bt(_self, _self.value);
@@ -88,8 +88,85 @@ ACTION realstate::reqbuypropt(uint64_t id, name buyer, asset amount)
     auto itr = pt.find(id);
     require_auth(buyer);
     eosio_assert(itr != pt.end(), "no available properties for this id");
+    eosio_assert(itr->owner != buyer, "you are already owner !!");
+
+    name rsdeposite = "rsdeposite11"_n;
+    string memo = "fund transfer";
+
+    buyer_table bt(_self, _self.value);
+    auto itr1 = bt.find(id);
+
+    if (itr1 == bt.end())
+    {
+        action(
+            permission_level{buyer, "active"_n},
+            "amartesttest"_n, "transfer"_n,
+            make_tuple(buyer, rsdeposite, amount, memo))
+            .send();
+        bt.emplace(_self, [&](auto &b) {
+            b.id = itr->propt_id;
+            b.buyername = buyer;
+            b.price = amount;
+        });
+    }
+    else
+    {
+        print("else part running !!!!!!!");
+        action(
+            permission_level{buyer, "active"_n},
+            "amartesttest"_n, "transfer"_n,
+            make_tuple(buyer, rsdeposite, amount, memo))
+            .send();
+        action(
+            permission_level{rsdeposite, "active"_n},
+            "amartesttest"_n, "transfer"_n,
+            make_tuple(rsdeposite, itr1->buyername, itr1->price, memo))
+            .send();
+
+        bt.modify(itr1, _self, [&](auto &b) {
+            b.buyername = buyer;
+            b.price = amount;
+        });
+    }
+}
+
+ACTION realstate::accbuyerreq(uint64_t id, name seller)
+{
+    properties_table pt(_self, _self.value);
+    auto itr = pt.find(id);
+    require_auth(seller);
+    eosio_assert(itr != pt.end(), "no available properties for this id");
+    eosio_assert(itr->owner == seller, "you are not valid owner of this property");
+
+    buyer_table bt(_self, _self.value);
+    auto itr1 = bt.find(id);
+    eosio_assert(itr != pt.end(), "no available buyer for this id");
+
+    name rsdeposite = "rsdeposite11"_n;
+    string memo = "fund transfer";
+
+    action(
+        permission_level{rsdeposite, "active"_n},
+        "amartesttest"_n, "transfer"_n,
+        make_tuple(rsdeposite, seller, itr1->price, memo))
+        .send();
+}
+
+ACTION realstate::reqsellpropt(uint64_t id, name seller, asset amount)
+{
+    properties_table pt(_self, _self.value);
+    auto itr = pt.find(id);
+    require_auth(seller);
+    eosio_assert(itr != pt.end(), "no available properties for this id !!");
+    eosio_assert(itr->owner == seller, "you are not valid owner of this property!!!!");
+    seller_table st(_self, _self.value);
+    st.emplace(_self, [&](auto &s) {
+        s.id = itr->propt_id;
+        s.sellername = seller;
+        s.sellingprice = amount;
+    });
 }
 
 /* ACTION realstate::reqbuypropt(uint64_t id,) */
 
-EOSIO_DISPATCH(realstate, (landproposal)(bid)(approvedprop)(reqbuypropt))
+EOSIO_DISPATCH(realstate, (landproposal)(bid)(approvedprop)(reqbuypropt)(reqsellpropt))
--- contracts/utopiarealstate/realstate.hpp
+++ contracts/utopiarealstate/realstate.hpp
@@ -10,6 +10,8 @@ CONTRACT realstate : public contract
     ACTION landproposal(string location, uint64_t area, name currentOwner, asset currentprice, uint64_t startdate, uint64_t enddate);
     ACTION bid(uint64_t id, name buyername,asset amount);
     ACTION approvedprop(uint64_t id);
+    ACTION reqbuypropt(uint64_t id, name buyer, asset amount);
+    ACTION reqsellpropt(uint64_t id,name seller,asset amount);
     TABLE bidtable
     {
         uint64_t id;
@@ -36,13 +38,20 @@ CONTRACT realstate : public contract
         bool citizen = false;
         uint64_t primary_key() const { return username.value; }
     };
-    // TABLE buyers
-    // {
-    //     uint64_t id;
-    //     name buyername;
-    //     asset price;
-    //     uint64_t primary_key() const { return id; }
-    // };
+    TABLE reqbuyers
+    {
+        uint64_t id;
+        name buyername;
+        asset price;
+        uint64_t primary_key() const { return id; }
+    };
+    TABLE reqsellers
+    {
+        uint64_t id;
+        name sellername;
+        asset sellingprice;
+        uint64_t primary_key() const { return id; }
+    };
     TABLE properties
     {
         uint64_t propt_id;
@@ -55,5 +64,6 @@ CONTRACT realstate : public contract
     typedef eosio::multi_index<"identity2"_n, identityt> identity_table;
     typedef multi_index<"bidtable"_n, bidtable> bid_table;
     typedef multi_index<"properties"_n, properties> properties_table;
-    // typedef multi_index<"buyertable"_n, realState> buyer_table;
+    typedef multi_index<"reqbuyertab"_n, reqbuyers> buyer_table;
+    typedef multi_index<"reqsellertab"_n, reqsellers> seller_table;
 };
