9589bb8dd293d1ebe4b48875cf1881ca9548e1d3
kstvigilante <kst85077@gmail.com>
2018-12-06 17:58:55 +0530

    changes to result page

--- dapp/Utopiaidentitydapp/imports/ui/pages/main/result.js
+++ dapp/Utopiaidentitydapp/imports/ui/pages/main/result.js
@@ -2,12 +2,12 @@ import "./result.html";
 import "../../stylesheets/result.css";
 import "./footer.js";
 import Eos from "eosjs";
-
+import { Session } from "meteor/session";
+import "./footer.js";
 
 
 eosConfig = {
     chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473", // 32 byte (64 char) hex string
-    keyProvider: ['5Jur4pK1Rb8xvdfNUUZJq5JE36HQUd9PNouWwjUdbWw7cK8ZuUo'],
     // WIF string or array of keys..
     httpEndpoint: 'https://jungle2.cryptolions.io:443',
     expireInSeconds: 60,
@@ -17,71 +17,7 @@ eosConfig = {
 }
 const eos = Eos(eosConfig);
 
-Template.App_result.helpers({
-    /* calcvote: async function () {
-        var result = [];
-        var length = 0;
-        var id = FlowRouter.current().params.id;
-        console.log("id: ", id);
-        let resultdata = await eos.getTableRows({
-            code: "voteproposal",
-            scope: "voteproposal",
-            table: "votes13",
-            limit: "50",
-            json: true,
-            key_type: "i64",
-            index_position: 2
-        });
 
-        let candidatedata = await eos.getTableRows({
-            code: "voteproposal",
-            scope: "voteproposal",
-            table: "proposal11",
-            limit: "50",
-            json: true,
-        });
-        console.log("resultdata ", resultdata);
-        console.log("candidate data: ", candidatedata);
-
-        //getting the length of list of all choices for a  particular proposal
-        var length = 0;
-        for (var i = 0; i < resultdata.rows.length; i++) {
-            if (id == resultdata.rows[i].proposal_id) {
-                length = resultdata.rows[i].choices.length;
-                break;
-            }
-        }
-
-        //creating a 2d array and setting initial votes to 0
-        for (var i = 0; i < length; i++) {
-            result[i] = [];
-        }
-
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length; j++) {
-                result[i][j] = 0;
-            }
-        }
-
-        var input = [];
-        //creating a 2d array of total votes received 
-        for (var i = 0; i < resultdata.rows.length; i++) {
-            if (id == resultdata.rows[i].proposal_id) {
-                input.push(resultdata.rows[i].choices)
-            }
-        }
-        for(var i=0;i<input.length;i++){
-            for(j=0;j<input[i].length;j++){
-                var val = input[i][j];
-                result[j][val-1] += 1;
-            }
-        }
-    
-        console.log("result after calculating ", result);
-        return result[0];
-
-    }, */
-})
 
 Template.App_result.onRendered(async function () {
     var result = [];
@@ -106,8 +42,7 @@ Template.App_result.onRendered(async function () {
         json: true,
     });
 
-    console.log("resultdata ", resultdata);
-
+   
     //getting the length of list of all choices for a  particular proposal
     var length = 0;
     for (var i = 0; i < resultdata.rows.length; i++) {
@@ -148,8 +83,8 @@ Template.App_result.onRendered(async function () {
     var desc;
     var proposaldetail;
 
-    for(var i=0;i<candidatedata.rows.length;i++){
-        if(id == candidatedata.rows[i].id){
+    for (var i = 0; i < candidatedata.rows.length; i++) {
+        if (id == candidatedata.rows[i].id) {
             desc = candidatedata.rows[i].proposal_description;
             proposaldetail = candidatedata.rows[i].proposal_detail;
             break;
@@ -161,34 +96,53 @@ Template.App_result.onRendered(async function () {
     document.getElementById("proposal-desc").innerHTML = desc;
     document.getElementById("proposal-detail").innerHTML = proposaldetail;
 
-    for(var i=0;i<candidatedata.rows.length;i++){
-        if(id == candidatedata.rows[i].id){
-            for(var j=0;j<candidatedata.rows[i].proposal_options.length;j++){
+    for (var i = 0; i < candidatedata.rows.length; i++) {
+        if (id == candidatedata.rows[i].id) {
+            for (var j = 0; j < candidatedata.rows[i].proposal_options.length; j++) {
                 var val = candidatedata.rows[i].proposal_options[j]
-                document.getElementById("proposal-result-name").innerHTML += "<br><div class = 'ep'>"+val+"</div>"
-    
+                document.getElementById("proposal-result-name").innerHTML += "<br><div class = 'ep'>" + val + "</div>"
+
             }
-                        
+
         }
     }
 
 
     console.log("candidatedata", candidatedata);
     console.log("result after calculating ", result);
-    
-    /* for(var i=0;i<result.length;i++){
-        document.getElementById("proposal-result-votes").innerHTML +="<br><div class = 'ep'>"+ result[i].toString().replace(/,/g, '/')+"</div>";
-        
-    } */
 
+    for (var i = 0; i < result.length; i++) {
+        document.getElementById("proposal-result-votes").innerHTML += "<br><div class = 'ep2'></div>";
 
-    for(var i=0;i<result.length;i++){
-        document.getElementById("proposal-result-votes").innerHTML +="<br><div class = 'ep2'></div>";
-
-        for(var j=0;j<result[i].length;j++){
+        for (var j = 0; j < result[i].length; j++) {
             var val = result[i][j]
-            document.getElementsByClassName("ep2")[i].innerHTML += "<div class = 'vote-stat'>"+val+"</div>"
+            document.getElementsByClassName("ep2")[i].innerHTML += "<div class = 'vote-stat'>" + val + "</div>"
         }
-        
+
+    }
+
+    var proposalResultList = await eos.getTableRows({
+        code: "voteproposal",
+        scope: "voteproposal",
+        table: "result13",
+        limit: "50",
+        json: true,
+    });
+
+    var obj;
+    for(var i=0;i<proposalResultList.rows.length;i++){
+        if(id == proposalResultList.rows[i].proposal_id){
+            obj = proposalResultList.rows[i];
+            console.log("obj ",obj);
+            document.getElementsByClassName("resultbox")[0].style.display = "block";
+            document.getElementsByClassName("title")[0].innerHTML = "Winners!"
+            for(var j=0;j<obj.selected.length;j++){
+                var val = obj.selected[j];
+                console.log("val ",val)
+                document.getElementsByClassName("selected")[0].innerHTML += "<div class = 'options'></div>";
+                document.getElementsByClassName("options")[j].innerHTML = "<p>"+val+"</p>";
+            }
+            
+        }
     }
 });
