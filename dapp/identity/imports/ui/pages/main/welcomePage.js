bbe71f6afff9ab27e403ae38282c6c61589c8f31
muskan soni <muskansoni@innotical.com>
2018-12-28 10:46:42 +0530

    modification

--- dapp/identity/imports/ui/pages/main/welcomePage.js
+++ dapp/identity/imports/ui/pages/main/welcomePage.js
@@ -17,7 +17,8 @@ const eosOptions = {
   chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473"
 };
 let eos = {};
-var manager=["propbudget11","identityreg1"];
+var userdetail;
+var manager=["propbudget11","identityreg1","realstateutp"];
 Template.welcomePage.onCreated(function bodyOnCreated() {
   ScatterJS.scatter.connect("utopia").then(connected => {
     if (connected) {
@@ -26,37 +27,32 @@ Template.welcomePage.onCreated(function bodyOnCreated() {
         const requiredFields = { accounts: [network] };
          eos = scatter.eos(network, Eos, eosOptions);
         if (scatter.identity) {
+          eos.getTableRows({
+            code: "identityreg1",
+            scope: "identityreg1",
+            table: "identity3",
+            limit: 50,
+            json: true
+          }).then((resp)=>{
+           userdetail=resp;
+           console.log("user---",userdetail);
+          });
           var username=localStorage.getItem("loginstatus");
-         /*  const acc = scatter.identity.accounts.find(
-            x => x.blockchain === "eos"
-          );
-          const account = acc.name;
-          localStorage.setItem("loginstatus", JSON.stringify(true));
-          localStorage.setItem("username", account);
-          console.log(
-            "inside created----1",
-            localStorage.getItem("loginstatus")
-          ); */
-          if(username ==manager[0] || username ==manager[1]){
+          if(username ==manager[0] || username ==manager[1] || username== manager[2]){
             console.log("1");
             document.getElementsByClassName("identitySectionman")[0].style.display = "flex";
             document.getElementById("managerText").style.display = "block";
+            var s = document.getElementById("len").setAttribute("value","manager");
           }else{
             console.log("2");
             document.getElementsByClassName("identitySectionman")[0].style.display = "flex";
             document.getElementById("managerText").style.display = "none";
+            var s = document.getElementById("len").setAttribute("value","userid");
                }  
           document.getElementById("loginButton").innerHTML = "logout";
           document.getElementsByClassName("optionFlex")[0].style.display =
             "flex";
         } else {
-         /*  localStorage.setItem("loginstatus", JSON.stringify(false));
-          localStorage.setItem("username", "");
-          console.log(
-            "inside created----2",
-            localStorage.getItem("loginstatus")
-          ); */
-         // document.getElementsByClassName("identitySection")[0].style.display = "none";
           document.getElementsByClassName("identitySectionman")[0].style.display = "none";
           document.getElementById("loginButton").innerHTML = "login";
           document.getElementsByClassName("optionFlex")[0].style.display ="none";
@@ -93,14 +89,16 @@ Template.welcomePage.events({
             document.getElementById("loginButton").innerHTML = "logout";
             document.getElementsByClassName("optionFlex")[0].style.display =
               "flex";
-              if(account ==manager[0] || account ==manager[1]){
+              if(account ==manager[0] || account ==manager[1] || account == manager[2]){
                 console.log("1");
                 document.getElementsByClassName("identitySectionman")[0].style.display = "flex";
                 document.getElementById("managerText").style.display = "block";
+                var s = document.getElementById("len").setAttribute("value","manager");
               }else{
                 console.log("2");
                 document.getElementsByClassName("identitySectionman")[0].style.display = "flex";
                 document.getElementById("managerText").style.display = "none";
+                var s = document.getElementById("len").setAttribute("value","userid");
                    }  
           })
           .catch(error => {
