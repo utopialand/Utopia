d236ef2a03ea2ee3ed25a861bbdc43e90b3e4ca6
muskan soni <muskansoni@innotical.com>
2018-12-28 12:40:57 +0530

    ---

--- dapp/identity/imports/ui/pages/main/realestate/realestate.js
+++ dapp/identity/imports/ui/pages/main/realestate/realestate.js
@@ -21,7 +21,7 @@ const eosOptions = {
 
 var scatter = {};
 var eosinstance = {};
-
+var userdetail;
 function allProperties(){
     ScatterJS.scatter.connect('utopia').then((connected) => {
         if (connected) {
@@ -40,7 +40,30 @@ function allProperties(){
                     }).then((response)=>{
                         console.log("response of all properties ", response.rows);
                         Session.set("allPropertyList", response.rows);
-                    });                  
+                    });
+                    eos.getTableRows({
+                        code: "identityreg1",
+                        scope: "identityreg1",
+                        table: "identity3",
+                        limit: 50,
+                        json: true
+                      }).then((resp)=>{
+                       userdetail=resp;
+                       console.log("user---",userdetail);
+                      });
+                      var username=localStorage.getItem("username");
+                      console.log("wlcm---",username);
+                      for(var i=0;i<userdetail.length;i++){
+                              if(userdetail.rows[i].username == username)
+                              {
+                                document.getElementsByClassName("manageproperty")[0].style.display = "flex";
+                                document.getElementsByClassName("buypropertypagebtn")[0].style.display = "flex";
+                                document.getElementsByClassName("bidpropertypagebtn")[0].style.display = "flex";         
+                              }
+                      }
+                    //   document.getElementsByClassName("identitySectionman")[0].style.display = "flex";
+                    //   document.getElementsByClassName("identitySectionman")[0].style.display = "flex";
+                    //   document.getElementsByClassName("identitySectionman")[0].style.display = "flex";         
                 }
                 else{
                     FlowRouter.go("/");
