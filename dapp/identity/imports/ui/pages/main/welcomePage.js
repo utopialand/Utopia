0a355ce85185b64fa0a26b53373f95f7bcf8a83c
Amar <amarjss0094@gmail.com>
2019-01-02 18:16:05 +0530

    new changes

--- dapp/identity/imports/ui/pages/main/welcomePage.js
+++ dapp/identity/imports/ui/pages/main/welcomePage.js
@@ -57,6 +57,7 @@ Template.welcomePage.onCreated(function bodyOnCreated() {
           document.getElementsByClassName("identitySectionman")[0].style.display = "none";
           document.getElementById("loginButton").innerHTML = "login";
           document.getElementsByClassName("optionFlex")[0].style.display ="none";
+          
         }
       }
     } else {
