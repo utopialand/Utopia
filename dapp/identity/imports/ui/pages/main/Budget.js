840d426b60dcb87bf23d1e7c4c74bef36df8fd8b
Amar <amarjss0094@gmail.com>
2019-01-04 14:29:34 +0530

    Budget page change

--- dapp/identity/imports/ui/pages/main/Budget.js
+++ dapp/identity/imports/ui/pages/main/Budget.js
@@ -23,7 +23,7 @@ let propentry;
 let winnerresult;
 Template.budget_app.onCreated(function() {
   Meteor.subscribe("identity");
-  ScatterJS.scatter.connect("utopia").then(connected => {
+  ScatterJS.scatter.connect("utopia").then(async connected => {
     if (connected) {
       if (ScatterJS.scatter.connect("utopia")) {
         scatter = ScatterJS.scatter;
@@ -31,17 +31,56 @@ Template.budget_app.onCreated(function() {
         const eos = scatter.eos(network, Eos, eosOptions);
         if (scatter.identity) {
           eosinstance = eos;
-          eos
-            .getTableRows({
+          budgetpropstart = await eosinstance.getTableRows({
+            code: "propbudget11",
+            scope: "propbudget11",
+            table: "votestat",
+            limit: 50,
+            json: true
+          });
+          propentry = await eosinstance.getTableRows({
+              code: "propbudget11",
+              scope: "propbudget11",
+              table: "feature112",
+              limit: 50,
+              json: true
+            });
+            winnerresult = await eosinstance.getTableRows({
+              code: "propbudget11",
+              scope: "propbudget11",
+              table: "result12",
+              limit: 50,
+              json: true
+            });
+            tabledata = await eosinstance.getTableRows({
               code: "propbudget11",
               scope: "propbudget11",
               table: "proposal13",
               limit: 50,
               json: true
-            })
-            .then(response => {
-              tabledata = response;
-              document.getElementById("largelist").innerHTML = "";
+            });
+            document.getElementById("budgetpropbutton").style.display = "flex";
+            if(propentry) {
+              console.log("propentry-----==", propentry);
+              console.log(propentry);
+              if (propentry.rows.length == 0) {
+                document.getElementById("stvvotingpage").style.display = "none";
+              }
+              else{
+                document.getElementById("stvvotingpage").style.display = "flex";
+              }
+            };
+            if(winnerresult) {
+              console.log("winner response!!==>", winnerresult);
+              if (winnerresult.rows.length == 0) {
+                document.getElementById("stvresultpage").style.display = "none";
+              }
+              else{
+                document.getElementById("stvresultpage").style.display = "flex";
+              }
+            };
+            console.log("tabledata ==",tabledata);
+            if(tabledata){
               var listlarge = [];
               var listmedium = [];
               var listsmall = [];
@@ -64,88 +103,54 @@ Template.budget_app.onCreated(function() {
                 }
               }
 
-              for (var i = 0; i < listlarge.length; i++) {
-                var desc = listlarge[i].proposal_description;
-                document.getElementById("largelist").innerHTML +=
-                  "<div class = 'redobudget'><p>" +
-                  desc +
-                  "</p><div class = 'like-button' id='" +
-                  listlarge[i].id +
-                  "' ></div>" +
-                  "</div>";
+              if(listlarge.length!=0)
+              {
+                document.getElementsByClassName("large")[0].style.display = "flex";
+                document.getElementById("largelist").innerHTML = "";
+                for (var i = 0; i < listlarge.length; i++) {
+                  var desc = listlarge[i].proposal_description;
+                  document.getElementById("largelist").innerHTML +=
+                    "<div class = 'redobudget'><p>" +
+                    desc +
+                    "</p><div class = 'like-button' id='" +
+                    listlarge[i].id +
+                    "' ></div>" +
+                    "</div>";
+                }
               }
-              document.getElementById("mediumlist").innerHTML = "";
-
-              for (var i = 0; i < listmedium.length; i++) {
-                var desc = listmedium[i].proposal_description;
-                document.getElementById("mediumlist").innerHTML +=
-                  "<div class = 'redobudget'><p>" +
-                  desc +
-                  "</p><div class = 'like-button' id='" +
-                  listmedium[i].id +
-                  "' ></div>" +
-                  "</div>";
+          
+              if(listmedium.length!=0)
+              {
+                document.getElementsByClassName("medium")[0].style.display = "flex";
+                document.getElementById("mediumlist").innerHTML = "";
+                for (var i = 0; i < listmedium.length; i++) {
+                  var desc = listmedium[i].proposal_description;
+                  document.getElementById("mediumlist").innerHTML +=
+                    "<div class = 'redobudget'><p>" +
+                    desc +
+                    "</p><div class = 'like-button' id='" +
+                    listmedium[i].id +
+                    "' ></div>" +
+                    "</div>";
+                }
               }
 
-              document.getElementById("smalllist").innerHTML = "";
-
-              for (var i = 0; i < listsmall.length; i++) {
-                var desc = listsmall[i].proposal_description;
-                document.getElementById("smalllist").innerHTML +=
-                  "<div class = 'redobudget'><p>" +
-                  desc +
-                  "</p><div class = 'like-button' id='" +
-                  listsmall[i].id +
-                  "' ></div>" +
-                  "</div>";
+              if(listsmall.length!=0)
+              {
+                document.getElementsByClassName("small")[0].style.display = "flex";
+                document.getElementById("smalllist").innerHTML = "";
+                for (var i = 0; i < listsmall.length; i++) {
+                  var desc = listsmall[i].proposal_description;
+                  document.getElementById("smalllist").innerHTML +=
+                    "<div class = 'redobudget'><p>" +
+                    desc +
+                    "</p><div class = 'like-button' id='" +
+                    listsmall[i].id +
+                    "' ></div>" +
+                    "</div>";
+                }
               }
-            });
-
-          eosinstance
-            .getTableRows({
-              code: "propbudget11",
-              scope: "propbudget11",
-              table: "votestat",
-              limit: 50,
-              json: true
-            })
-            .then(resp => {
-              budgetpropstart = resp;
-              console.log(budgetpropstart);
-            });
-
-          eosinstance
-            .getTableRows({
-              code: "propbudget11",
-              scope: "propbudget11",
-              table: "feature112",
-              limit: 50,
-              json: true
-            })
-            .then(resp => {
-              propentry = resp;
-              console.log("propentry-----", resp);
-              console.log(propentry);
-              if (propentry.rows.length == 0) {
-                document.getElementById("stvvotingpage").style.display = "none";
-              }
-            });
-
-          eosinstance
-            .getTableRows({
-              code: "propbudget11",
-              scope: "propbudget11",
-              table: "result12",
-              limit: 50,
-              json: true
-            })
-            .then(resp => {
-              winnerresult = resp;
-              console.log("winner response!!==>", resp);
-              if (winnerresult.rows.length == 0) {
-                document.getElementById("stvresultpage").style.display = "none";
-              }
-            });
+            };
         } else {
           FlowRouter.go("/");
         }
@@ -159,7 +164,7 @@ Template.budget_app.onCreated(function() {
 Template.budget_app.onRendered(async function() {});
 Template.budget_app.events({
   ////click on like button to see response
-  "click .like-button": function() {
+  "click .like-button": async function() {
     if (
       budgetpropstart.rows[0] == null ||
       budgetpropstart.rows[0].status == false
@@ -172,17 +177,26 @@ Template.budget_app.events({
       document.getElementsByClassName("like-button")[0].style.display = "block";
       var propid = event.target.id;
       var username = localStorage.getItem("username");
-      eosinstance.contract("propbudget11").then(propbudget11 => {
-        propbudget11
-          .catgvote(propid, username, { authorization: username })
-          .then(response => {
-            if (response) {
-              alert("budget voting successfull");
-            } else {
-              alert("something went wrong!!!!");
-            }
-          });
-      });
+      try{
+        let propbudget11 = await eosinstance.contract("propbudget11");
+        if(propbudget11)
+        {
+          let result = await propbudget11.catgvote(propid, username, { authorization: username });
+            if(result){
+              if (response) {
+                alert("budget voting successfull");
+              } else {
+                alert("something went wrong!!!!");
+              }
+            };
+        };
+      }
+      catch(err)
+      {
+        var parseResponse = await JSON.parse(err);
+        var msg = await parseResponse.error.details[0].message.split(":")[1]
+        alert(msg);
+      }
     }
   },
   "click .budgetpropbutton": function() {
