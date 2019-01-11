8fee9def352225cfdc1416b5730649be5dd5d7ae
muskan soni <muskansoni482@gmail.com>
2019-01-11 10:53:04 +0530

    maturity field

--- dapp/identity/imports/ui/pages/main/Buybond.js
+++ dapp/identity/imports/ui/pages/main/Buybond.js
@@ -37,7 +37,7 @@ Template.buybond.onCreated(function () {
             document.getElementById("bond-group").innerHTML = "";                     
             for (var i = 0; i < tabledata.rows.length; i++) {
                 var bond = tabledata.rows[i].bond;
-                var maturity = tabledata.rows[i].maturitycount;
+                var maturity = tabledata.rows[i].maturity;
                 var couponrate = tabledata.rows[i].couponrate;
                 var couponintervel = tabledata.rows[i].couponintervel;
                 var facevalue = tabledata.rows[i].facevalue;
